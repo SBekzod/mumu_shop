3461d04046d7ccc72835228b46eacb20fac268c0
Martin <bekzodsanakulov89@gmail.com>
2021-04-28 10:26:27 +0900

    feat: AppBar NAWC's action argument to create menu

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -7,6 +7,24 @@ class ProductsOverviewScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (int selectedValue) {
+              print(selectedValue);
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (ele) => [
+              PopupMenuItem(
+                child: Text('Only favorites', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20),),
+                value: 0,
+              ),
+              PopupMenuItem(
+                child: Text('Show all', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20),),
+                value: 1,
+              )
+            ],
+          )
+        ],
       ),
       body: ProductsGridSample(),
     );
