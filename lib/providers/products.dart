cf1af3d4f38e1011d2f00d1844128f1243c91542
Martin <bekzodsanakulov89@gmail.com>
2021-04-30 15:13:26 +0900

    fix: hard adding products state

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -5,6 +5,38 @@ class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
+      title: 'Books',
+      description: 'Prepare yourself for life.',
+      price: 749.99,
+      imageUrl:
+      'https://d2r68eeixpqexd.cloudfront.net/41fd2ced63aa8d47a3142fa4cd46849b.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Playstation',
+      description: 'Prepare yourself for game.',
+      price: 949.99,
+      imageUrl:
+      'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Untitled1578.png',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Ball',
+      description: 'Prepare yourself for sports.',
+      price: 19.99,
+      imageUrl:
+      'https://soccerballworld.com/wp-content/uploads/2020/04/alphabet-word-images-1295331_640.png',
+    ),
+    Product(
+      id: 'p4',
+      title: 'Cyber truck',
+      description: 'Prepare yourself for life.',
+      price: 59999.00,
+      imageUrl:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-tesla-cyber-truck-mmp-1-1579127142.jpg',
+    ),
+    Product(
+      id: 'p5',
       title: 'Shirt',
       description: 'A red shirt - it is pretty red!',
       price: 79.99,
@@ -12,7 +44,7 @@ class Products with ChangeNotifier {
           'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
-      id: 'p2',
+      id: 'p6',
       title: 'Trousers',
       description: 'A nice pair of trousers.',
       price: 59.99,
@@ -20,7 +52,7 @@ class Products with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
-      id: 'p3',
+      id: 'p7',
       title: 'Yellow Scarf',
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
@@ -28,7 +60,7 @@ class Products with ChangeNotifier {
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
-      id: 'p4',
+      id: 'p8',
       title: 'A Pan',
       description: 'Prepare any meal you want.',
       price: 49.99,
