169ffa03c1b054f6c88c5f809a60b7833611236b
Martin <martin@comets.kr>
2021-05-03 00:23:59 +0900

    feat: creating CartItem and Cart class providers using Dart Maps

---
+++ lib/providers/cart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.price,
+      @required this.quantity,
+      @required this.title});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addCartItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (target) => CartItem(
+              id: target.id,
+              price: target.price,
+              quantity: target.quantity + 1,
+              title: target.title));
+    } else {
+      _items[productId] = CartItem(
+          id: DateTime.now().toString(),
+          price: price,
+          quantity: 1,
+          title: title);
+    }
+  }
+}
